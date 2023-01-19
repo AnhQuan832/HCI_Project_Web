83364b050ef8ad037d0629a828035e41f0c5b4b2
PHoangLe <lehoangphuc180502@gmail.com>
2023-01-19 23:28:54 +0700

    thêm header và navbar component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { faBookOpen,
+  faSquareCheck,
+  faUserGraduate } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +9,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.less']
 })
 export class HeaderComponent {
+  faUserGraduate = faUserGraduate;
 
 }
