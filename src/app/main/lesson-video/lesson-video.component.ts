dcafef39d392536956962072cc21264dc97ec4d8
PHoangLe <lehoangphuc180502@gmail.com>
2023-01-29 17:55:42 +0700

    thêm video vào assets folder và lấy thời gian hiện tại của video

--- src/app/main/lesson-video/lesson-video.component.ts
+++ src/app/main/lesson-video/lesson-video.component.ts
@@ -10,15 +10,25 @@ export class LessonVideoComponent implements OnInit {
 
   breadcrumbs: MenuItem[];
 
+  video: HTMLVideoElement = document.getElementById('video') as HTMLVideoElement;
+
   constructor() {
     this.breadcrumbs = [
       {label: 'Bài học', routerLink: "/main/lesson"},
       {label: 'Bài 1: Phương trình lượng giác'},
     ];
-
   }
 
   ngOnInit() {
+    this.video = document.getElementById('video') as HTMLVideoElement;
+
+    this.video.addEventListener('timeupdate', (event) =>{
+      this.setTimeStamp();
+    })
+  }
+
+  setTimeStamp(){
+    (document.getElementById("test-btn") as HTMLButtonElement).innerHTML = (this.video.currentTime).toString();
   }
 
 }
